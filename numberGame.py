323546b70cc2a42df0c7997ffe18e50460f3c505
Shamanth M <162690888+shamanth74@users.noreply.github.com>
2024-03-16 21:59:27 +0530

    Update numberGame.py

--- numberGame.py
+++ numberGame.py
@@ -1,100 +1,59 @@
 #This is a random Number guessing game...
-#Made without the use of functions 
-#Used only conditional and looping statments
+#Made with the use of functions 
+
 
 import random as r
-count=1
+
+# a=max_count,  b=sol  max=highest number
+
+def guess(a,b,max):
+    c=r.randint(0,max)
+    count=1
+    while count<=a and b!=c:
+        attempt=str(count+1)
+        
+        if b>c:
+            print("Lower number ")
+            b=int(input("Take your guess >> "))
+        elif b<c:
+            print("Higher number")
+            b=int(input("Take your guess >> "))
+        
+        count+=1
+    if b==c and count<=a:
+        print("You Won..!\nYou guessed in "+attempt+" attempts")
+    elif count>a:
+        actual=str(c)
+        print("\nThe number was "+actual+" ")
+        print("\nYou Lost..Exceeded maximum attempts\nBetter Luck next time")
+
+
+
 print("Number Game \n")
 print("1.Easy (b/w 0 & 200) 30-Chances\n2.Medium (b/w 0 & 700) 25-Chances\n3.Difficult (b/w 0 & 1000) 20-Chances\n4.Hard (b/w 0 & 3000) 15-Chances")
 num=int(input("Enter your choice >> "))
-print("\nGame starts.. Enjoy it ....\n")
-sol=int(input("Take your guess >> "))
+
+if num>=1 and num<=4:
+    print("\nGame starts.. Enjoy it ....\n")
+
 if num==1:
-    x=r.randint(0,200)
-    # count=30
-    while count<=30 and sol!=x:
-        attempt=str(count+1)
-        
-        if sol>x:
-            print("Lower number ")
-            sol=int(input("Take your guess >> "))
-        elif sol<x:
-            print("Higher number")
-            sol=int(input("Take your guess >> "))
-        
-        count+=1
-    if sol==x and count<=30:
-        print("You Won..!\nYou guessed in "+attempt+" attempts")
-    elif count>30:
-        actual=str(x)
-        print("\nThe number was "+actual+" ")
-        print("\nYou Lost..Exceeded maximum attempts\nBetter Luck next time")
+   sol=int(input("Take your guess >> "))
+    guess(30,sol,200)
 
 
 elif num==2:
-    x=r.randint(0,700)
-    # count=25
-    while count<=25 and sol!=x:
-        attempt=str(count+1)
-        
-        if sol>x:
-            print("Lower number ")
-            sol=int(input("Take your guess >> "))
-        elif sol<x:
-            print("Higher number")
-            sol=int(input("Take your guess >> "))
-        
-        count+=1
-    if sol==x and count<=25:
-        print("You Won..!\nYou guessed in "+attempt+" attempts")
-    elif count>25:
-        actual=str(x)
-        print("\nThe number was "+actual+" ")
-        print("\nYou Lost..Exceeded maximum attempts\nBetter Luck next time")
+    sol=int(input("Take your guess >> "))
+    guess(25,sol,700)
 
 
 elif num==3:
-    x=r.randint(0,1000)
-    # count=20
-    while count<=20 and sol!=x:
-        attempt=str(count+1)
-        
-        if sol>x:
-            print("Lower number ")
-            sol=int(input("Take your guess >> "))
-        elif sol<x:
-            print("Higher number")
-            sol=int(input("Take your guess >> "))
-        
-        count+=1
-    if sol==x and count<=20:
-        print("You Won..!\nYou guessed in "+attempt+" attempts")
-    elif count>20:
-        actual=str(x)
-        print("\nThe number was "+actual+" ")
-        print("\nYou Lost..Exceeded maximum attempts\nBetter Luck next time")
+    sol=int(input("Take your guess >> "))
+    guess(20,sol,1000)
 
 
 elif num==4:
-    x=r.randint(0,3000)
-    # count=15
-    while count<=15 and sol!=x:
-        attempt=str(count+1)
-        
-        if sol>x:
-            print("Lower number ")
-            sol=int(input("Take your guess >> "))
-        elif sol<x:
-            print("Higher number")
-            sol=int(input("Take your guess >> "))
-        
-        count+=1
-    if sol==x and count<=15:
-        print("You Won..!\nYou guessed in "+attempt+" attempts")
-    elif count>15:
-        actual=str(x)
-        print("\nThe number was "+actual+" ")
-        print("\nYou Lost..Exceeded maximum attempts\nBetter Luck next time")
+    sol=int(input("Take your guess >> "))
+    guess(15,sol,3000)
         
 
 
